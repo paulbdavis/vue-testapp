0d1abade750f978d355240dc591c8997f70e3b03
paulbdavis <paul@dangersalad.com>
2018-11-15 15:36:33 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <main>
+            <v-content>
+                <v-container>
+                    <v-layout row wrap>
+                        <lib-ui-thing>
+                            <h1 slot="header">Test App</h1>
+                            <lib-thing message="I am a test app."/>
+                        </lib-ui-thing>
+                    </v-layout>
+                </v-container>
+            </v-content>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
